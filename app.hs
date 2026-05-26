0760bc689735ea80c1a57cb5f03e8d12064d93a6
Soleil <pedrovinimg1@gmail.com>
2026-05-26 12:35:57 -0300

    Conversor de Strings, Leitura e Escrita de Arquivos

--- app.hs
+++ app.hs
@@ -1,2 +1,20 @@
+module Main where
+
+strConvert :: String -> String
+strConvert str = "<p>" ++ str ++ "</p>"
+
+
+processStr :: String -> String
+processStr bruteStr = unlines (map strConvert (lines bruteStr))
+
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+    content <- readFile "content/post.txt"
+    
+    let htmlFinal = processStr content
+
+    writeFile "dist/post.html" htmlFinal
+
+    putStrLn "Site estático gerado com sucesso em dist/post.html" 
+
